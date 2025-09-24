aa206c31465ece43fc2259a2b0e990f1f4583947
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-25 00:06:42 +0200

    card component and scss created

---
+++ src/components/ui/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../styles/components/_card.scss";
+
+export function Card({ className = "", children, ...props }) {
+  return (
+    <div className={`card ${className}`} {...props}>{children}</div>
+  );
+}
+
+export function CardHeader({ className = "", children }) {
+  return <div className={`card__header ${className}`}>{children}</div>;
+}
+
+export function CardTitle({ className = "", children }) {
+  return <h3 className={`card__title ${className}`}>{children}</h3>;
+}
+
+export function CardContent({ className = "", children }) {
+  return <div className={`card__content ${className}`}>{children}</div>;
+}
+
+export function CardFooter({ className = "", children }) {
+  return <div className={`card__footer ${className}`}>{children}</div>;
+}
