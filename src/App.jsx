c31febfed382cea09c689d39ccbda07d57ffddb9
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-26 16:58:15 +0200

    manager route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import "./styles/base.scss";
+import ManagerDashboard from "./pages/ManagerDashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/manager" element={<ManagerDashboard />} />
       </Routes>
     </Router>
   );
