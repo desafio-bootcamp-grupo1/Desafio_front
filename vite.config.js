88fa72bf8645de45486574003c4a181bdcd66930
eduardolm1 <eduardolm300601@gmail.com>
2025-09-29 19:26:28 +0200

    Corrigiendo archivos vercel

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   server: {
     port: 5173,
     strictPort: true,
-    proxy: {
-      '/api': {
-        target: '',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
   },
   build: {
     outDir: 'dist'
