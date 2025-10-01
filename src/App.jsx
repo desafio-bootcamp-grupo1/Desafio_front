baf687b5028489366a1a614f821f46659782bf82
Nahia Otegui <nay.fullstack@gmail.com>
2025-10-01 12:16:55 +0200

    cookies agregadas

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import ManagerDashboard from "./pages/ManagerDashboard";
 import DriverDashboard from "./pages/DriverDashboard"; 
 import EscanerPage from "./pages/EscanerPage";
 import Camera from "./components/camera/Camera";
+import CookiesPage from "./pages/Cookies";
 import { Toaster } from "react-hot-toast";
+import CookiePopup from "./components/common/CookiesPopup";
 import "./styles/base.scss";
 
 function App() {
@@ -28,7 +30,9 @@ function App() {
           <Route path="/app/manager" element={<ManagerDashboard />} />
           <Route path="/app/" element={<EscanerPage />} />
           <Route path="/app/camera" element={<Camera />} />
+          <Route path="/cookies" element={<CookiesPage />} />
         </Routes>
+        <CookiePopup />
       </Router>
     </>
   );
--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import auth from "../features/auth/authSlice";
 import driverProfile from "../features/auth/driverProfileSlice";
+import cookiesReducer from "../features/auth/cookiesSlice";
 
 
 export const store = configureStore({
   reducer: {
-    auth,
-    driverProfile,     
+  auth,
+  driverProfile,
+   cookies: cookiesReducer, 
 
   },
 });
